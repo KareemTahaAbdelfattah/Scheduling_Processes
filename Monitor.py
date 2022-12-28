5c603278810b0f43de96c482df2b8a3f808be5a7
Kareem Taha <82422224+KareemTahaAbdelfattah@users.noreply.github.com>
2022-12-28 11:08:47 +0200

    Create Monitor.py

---
+++ Monitor.py
@@ -0,0 +1,30 @@
+import psutil
+import schedule
+import time
+
+print(psutil.pids()  )
+pids = psutil.pids()
+#p = psutil.Process( pids[3] )
+#print(p)
+print("|" +  ("-"*10 + "|" )*30 )
+print("|" +  ("-"*10 + "|" )*30 )
+psutil.process_iter(attrs=None, ad_value=None)
+def display():
+    for proc in psutil.process_iter():
+        try:
+            # Get process name & pid from process object.
+            processName = proc.name()
+            processID = proc.pid
+            ProcessStatus = proc.status();
+            #ProcessMemoryInfo = proc.memory_info
+            print("Name ----> '", processName,"', Pid = ", processID, ", Status = ", ProcessStatus)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+display()
+print("|" +  ("-"*10 + "|" )*30 )
+schedule.every(15).seconds.do(display)
+print("|" +  ("-"*10 + "|" )*30 )
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
